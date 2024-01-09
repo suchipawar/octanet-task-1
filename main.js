dec08916db614ecac8931e2e50ea026d888738bd
suchipawar <153375590+suchipawar@users.noreply.github.com>
2024-01-09 20:57:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+(function ($) {
+    "use strict";
+   
+    // Testimonials carousel
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+
